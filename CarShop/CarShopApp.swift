48aca3046c37eee49e3177c9ac7fe3601009e17c
Michael <hayrapetyanmichael1@gmail.com>
2026-04-22 21:19:32 +0400

    Initial Commit

---
+++ CarShop/CarShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarShopApp.swift
+//  CarShop
+//
+//  Created by Michael Hayrapetyan on 22.04.26.
+//
+
+import SwiftUI
+
+@main
+struct CarShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarShop
+//
+//  Created by Michael Hayrapetyan on 22.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
